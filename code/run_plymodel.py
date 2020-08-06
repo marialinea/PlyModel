6b9fb1b00059b27e580cce01f094b4d26dc63dfd
marialinea <marialinea1@hotmail.com>
2020-08-06 11:10:04 +0200

    Uploaded test program

---
+++ code/run_plymodel.py
@@ -0,0 +1,23 @@
+"""
+This is a test program designed to take a .ply file and its path as
+input from the command line and run check whether is it ready for 
+rendering through the PlyModel class. 
+"""
+
+
+import PlyModel as ply
+
+filename = input("Filename or path to .ply file: \n")
+
+words = filename.split(".")
+if words[-1] != "ply":
+    print("The given file is not a .ply file.")
+    
+else:
+    
+    instance = ply.PlyModel(filename)                # create class instance
+    
+    instance.ReadPly()                               
+    instance.LooseGeometry()                         
+    instance.GroupIdentification()
+    instance.PolygonOrientation()
